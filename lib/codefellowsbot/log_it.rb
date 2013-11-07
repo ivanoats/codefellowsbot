680fb6af58128a5b5218e2418a5ffebfc6eb1ed0
Ivan Storck <ivanoats@gmail.com>
2013-11-06 22:56:53 -0800

    remove terrible mod 5 uploader

--- lib/codefellowsbot/log_it.rb
+++ lib/codefellowsbot/log_it.rb
@@ -4,9 +4,6 @@ module CodeFellowsBot
       File.open("./codefellows_chat_log.txt","a") do |file|
         file.puts "#{user}: #{message}"
       end
-      if (Time.now.min % 5 == 0)
-        S3Uploader.upload_log
-      end
     end
   end
 end
