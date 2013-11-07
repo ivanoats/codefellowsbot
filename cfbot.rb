4d41e543cbd027a9555547e633afa1e4c7671ab8
Ivan Storck <ivanoats@gmail.com>
2013-11-06 17:47:37 -0800

    start of amazon uploading of logs

--- cfbot.rb
+++ cfbot.rb
@@ -5,7 +5,33 @@ module LogIt
     File.open("./codefellows_chat_log.txt","a") do |file|
       file.puts "#{user}: #{message}"
     end
+    if (Time.now.min % 5 == 0)
+      S3Uploader.upload_log
+    end
   end
+end
+
+class S3Uploader
+  require 'aws/s3'
+
+  def self.upload_log
+    upload_file('./codefellows_chat_log.txt')
+  end
+
+private
+  def establish_connection
+    AWS:S3::Base.establish_connection!(
+      :access_key_id => ENV['CF_AWS_KEY'],
+      :secret_access_key => ENV['CF_AWS_SECRET']
+    )
+  end
+
+  def upload_file(filename)
+    establish_connection
+    S3Object.store(filename, open('filename'), 'assets.codefellows.org')
+    puts "stored S3 object: #{filename}"
+  end
+
 end
 
 class CFLogger
