06229b583c70de3f4e9aa05b815678b60a280aeb
Ivan Storck <ivanoats@gmail.com>
2013-11-07 13:45:26 -0800

    adding paragraph to log lines

--- lib/codefellowsbot/log_it.rb
+++ lib/codefellowsbot/log_it.rb
@@ -6,7 +6,7 @@ module CodeFellowsBot
 
     def log_it(user, message)
       File.open(log_file_name,"a") do |file|
-        file.puts "<small>(#{Time.now.asctime})</small> <b>#{user}</b>: #{message}"
+        file.puts "<p><small>(#{Time.now.asctime})</small> <b>#{user}</b>: #{message}</p>"
       end
     end
   end
