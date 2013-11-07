2b482461bc0404fb7e560b0d91decd91cd8c3b15
Ivan Storck <ivanoats@gmail.com>
2013-11-06 22:29:58 -0800

    adding bin dir

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,9 @@
 require 'minitest/autorun'
 require 'minitest/colorize'
+require 'minitest/spec'
+
+# Always use local CodeFellowsBot first
+this_dir = File.join(File.dirname(__FILE__), "..")
+$LOAD_PATH.unshift File.expand_path(this_dir)
+
+require "codefellowsbot"
